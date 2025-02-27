45c7b839161b04e3b4332e429d8af12ca29ed529
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-02-27 22:26:01 +0800

    feat(new-branch): added react-native branch

--- screen/index.js
+++
@@ -1,6 +0,0 @@
-import Chat from "./Chat";
-import Chats from "./Chats";
-import Intro from "./Intro";
-import Welcome from "./Welcome";
-
-export { Chat, Chats, Intro, Welcome };
---
+++ src/app/(auth)/terms_agreement.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const TermsAgreement = () => {
+  return (
+    <View>
+      <Text>TermsAgreement</Text>
+    </View>
+  );
+};
+
+export default TermsAgreement;
