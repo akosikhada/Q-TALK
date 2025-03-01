f29b306cfe5903a0bd9e467dd06256fb98b2846a
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-03-01 12:24:28 +0800

    refactor(button): enhance ButtonComponent to accept custom styles
    - Updated ButtonComponent to accept custom styles through props
    - Applied custom styles to button container and text
    - Renamed 'style' to 'styles' in StyleSheet for better clarity

--- src/components/atoms/ButtonComponent.tsx
+++ src/components/atoms/ButtonComponent.tsx
@@ -2,21 +2,21 @@ import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import React from "react";
 import { moderateScale, scale, verticalScale } from "react-native-size-matters";
 
-const ButtonComponent = ({ title, onPress }: any) => {
+const ButtonComponent = ({ title, onPress, style }: any) => {
   return (
     <TouchableOpacity
-      style={style.button_container}
+      style={[styles.button_container, style]}
       activeOpacity={0.8}
       onPress={onPress}
     >
-      <Text style={style.button_text}>{title}</Text>
+      <Text style={styles.button_text}>{title}</Text>
     </TouchableOpacity>
   );
 };
 
 export default ButtonComponent;
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   button_container: {
     backgroundColor: "#00a884",
     alignItems: "center",
