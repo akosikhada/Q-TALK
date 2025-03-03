3c8f98f9ded0d504a68777c993773b9971d2fab8
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-03-03 10:39:03 +0800

    refactor(main): update MainStack component to hide header
    - Added screenOptions to Stack component with headerShown set to false
    - Defined Stack.Screen for 'index' route

--- src/app/(main)/_layout.tsx
+++ src/app/(main)/_layout.tsx
@@ -2,7 +2,11 @@ import { Stack } from "expo-router";
 import React from "react";
 
 const MainStack = () => {
-  return <Stack></Stack>;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
 };
 
 export default MainStack;
