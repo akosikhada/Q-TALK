59384f1f2ce5b53eaa5ee6821fcb7995c64770d4
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-03-03 10:40:33 +0800

    feat(auth): enhance RootNavigation component with access token check
    - Added storage import from utils/storage
    - Implemented access token check in useEffect to set login state
    - Updated SplashScreen logic to hide after access token verification

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { Redirect, Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
+import { storage } from "../utils/storage";
 
 SplashScreen.preventAutoHideAsync();
 const RootNavigation = () => {
   const [isLogin, setIsLogin] = useState(false);
+  const access_token = storage.getString("access_token");
   useEffect(() => {
     SplashScreen.hideAsync();
   }, []);
+
+  useEffect(() => {
+    console.log(access_token);
+    if (access_token) {
+      setIsLogin(true);
+    }
+  }, [access_token]);
+
   return (
     <>
       <Stack screenOptions={{ headerShown: false }} />
