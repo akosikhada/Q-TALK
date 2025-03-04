47bfaadae7bb7719535a4ddf445c2d7036c16bdd
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-03-04 10:15:09 +0800

    feat(molecules): enhance Chats component with message mock data and MessageCard integration
    - Added mock data for messages with profile images, user names, message content, time, and count
    - Integrated MessageCard component to display individual message items
    - Implemented FlatList to render messages dynamically

--- src/components/molecules/Chats.tsx
+++ src/components/molecules/Chats.tsx
@@ -1,10 +1,42 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import React from "react";
+import MessageCard from "./MessageCard";
+import ImagePath from "@/src/constants/ImagePath";
 
 const Chats = () => {
+  const messages_data_mock_up = [
+    {
+      profile_images: ImagePath.q_talk_logo,
+      users_name: "Mark Angelo Gulpe",
+      message_content: "Hello, World!",
+      message_time: "10:01 AM",
+      message_count: "1",
+    },
+    {
+      profile_images: ImagePath.q_talk_logo,
+      users_name: "Miguel Enrique Dasalla",
+      message_content: "Hello, Motherfucker!",
+      message_time: "10:01 AM",
+      message_count: "2",
+    },
+  ];
+
   return (
-    <View style={{ flex: 1, backgroundColor: "blue" }}>
-      <Text>Chats</Text>
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={messages_data_mock_up}
+        renderItem={({ item }) => {
+          return (
+            <MessageCard
+              profile_images={item.profile_images}
+              users_name={item.users_name}
+              message_content={item.message_content}
+              message_time={item.message_time}
+              message_count={item.message_count}
+            />
+          );
+        }}
+      />
     </View>
   );
 };
