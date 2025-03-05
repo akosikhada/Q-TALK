70599c18b91eeecf5a4f2d5d84e88c2836a0a6d2
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-03-05 20:35:51 +0800

    refactor(constants): update image constants with consistent naming
    - Renamed image constants for consistency (e.g., q_talk_logo to qTalkLogo)
    - Updated image paths accordingly

--- src/constants/ImagePath.ts
+++ src/constants/ImagePath.ts
@@ -1,4 +1,4 @@
 export default {
-  q_talk_logo: require("@/src/assets/images/Q-TALK-LOGO-SIMPLE.png"),
-  q_talk_welcome: require("@/src/assets/images/welcome-semi-round.png"),
+  qTalkLogo: require("@/src/assets/images/Q-TALK-LOGO-SIMPLE.png"),
+  qTalkWelcome: require("@/src/assets/images/welcome-semi-round.png"),
 };
