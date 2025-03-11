65bfd87842dc060bea955597997f58d5aaf461e7
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-03-11 23:03:43 +0800

    feat(modals): implement and export multiple modals for permissions and privacy
    - Added new modal components: BlockedContactActionModal, CallsPermissionModal, GroupAddPermissionModal, MessageRetentionModal, and ProfilePhotoPrivacyModal
    - Updated index.ts to export newly created modal components for modularity and reusability

---
+++ components/modals/index.ts
@@ -0,0 +1,5 @@
+export { default as ProfilePhotoPrivacyModal } from "./ProfilePhotoPrivacyModal";
+export { default as GroupAddPermissionModal } from "./GroupAddPermissionModal";
+export { default as CallsPermissionModal } from "./CallsPermissionModal";
+export { default as MessageRetentionModal } from "./MessageRetentionModal";
+export { default as BlockedContactActionModal } from "./BlockedContactActionModal";
