3dcfe7fcfe98875be6dab31f898373a80003f4b2
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-03-11 23:08:24 +0800

    feat(components): add index.ts for centralized component exports
    - Created index.ts to streamline imports for components
    - Exported BottomNavBar, CustomAlert, MessageActionModal, StyledComponents, and modals
    - Improved modularity and maintainability for component imports

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+// Export components for easier imports
+export { default as BottomNavBar } from "./BottomNavBar";
+export { default as CustomAlert } from "./CustomAlert";
+export { default as MessageActionModal } from "./MessageActionModal";
+export * from "./StyledComponents";
+export * from "./modals";
